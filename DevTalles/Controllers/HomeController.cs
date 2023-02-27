66463fd99c844bc1bc499f4aa50993433b8c1e92
Daniel <Daniel@DESKTOP-B7DII6F>
2023-02-27 12:52:52 -0600

    Agregar archivos de proyecto.

---
+++ DevTalles/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using DevTalles.Data;
+using DevTalles.Models;
+using DevTalles.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace DevTalles.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext db;
+
+        public HomeController(ILogger<HomeController> logger , ApplicationDbContext db)
+        {
+            _logger = logger;
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            HomeVM model = new()
+            {
+                Cursos = db.Cursos.Include(c => c.Categoria).Include(sb => sb.SubCategoria),
+                Categorias = db.Categorias.ToList(),
+                CursosDisponibles = db.Cursos.Any()
+            };
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
